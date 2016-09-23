2a83f5730c4d8b67247e5b712da85791bb598f32
Henrik René Høegh <heh@praqma.net>
2016-09-23 15:20:01 +0200

    corrected name of deployment

--- henrik/nodeport-loadbalancer/start-pods.sh
+++ henrik/nodeport-loadbalancer/start-pods.sh
@@ -1,6 +1,6 @@
 echo "
 Starting pod with NginX"
-kubectl run my-apache --image=kamranazeem/centos-multitool --replicas=2 --port=80
+kubectl run my-NginX --image=kamranazeem/centos-multitool --replicas=2 --port=80
 
 echo "
 Starting service to expose NginX service as NodePort"
