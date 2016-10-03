f5c1be0d64a151b8e9c2e9b92e66ba44173e10ae
Henrik René Høegh <heh@praqma.net>
2016-10-03 12:52:43 +0200

    Fixed from bal to bl in delete/cleanup section

--- henrik/nodeport-loadbalancer/run.sh
+++ henrik/nodeport-loadbalancer/run.sh
@@ -5,7 +5,7 @@ echo " - Running createLoadBalancer"
 createLoadBalancer
 
 echo " - Cleaning up old files"
-rm -f /etc/httpd/conf.d/*.bal
+rm -f /etc/httpd/conf.d/*.bl
 
 echo " - Copying files"
 mv -f kubernetes.services.conf /etc/httpd/conf.d/
