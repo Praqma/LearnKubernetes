0ed2b1110ea6b68753aa9abdfdec0cca422213cb
Henrik René Høegh <heh@praqma.net>
2016-10-04 14:31:16 +0200

    Added HAProxy config as output

--- henrik/nodeport-loadbalancer/tools.f
+++ henrik/nodeport-loadbalancer/tools.f
@@ -1,13 +1,104 @@
 source ../apiReader/apiReader.f
 
 function createLoadBalancer(){
+  local LBType=$1
+
+  # Setting LBType to apache, if not specified
+  if [ -z "$LBType" ];then
+     echo "Setting Loadbalancer type to Apache (default"
+     LBType="apache"
+  fi
+
+  rm -f *.conf 
+  rm -f *.bl 
+
+  if [ "$LBType" = "apache" ]; then
+    createLBApache
+  elif [ "$LBType" = "haproxy"  ]; then
+    createLBHaproxy
+  fi
+} 
+
+function createLBHaproxy(){
   local Services=$(getServices default | tr " " "\n")
   local Nodes=$(getNodeNames)
   local nodeIP=""
   local line=""
 
-  rm -f *.conf 
-  rm -f *.bl 
+  echo "global
+        stats timeout 30s" > haproxy.conf
+
+  echo "defaults
+        log     global
+        mode    http
+        option  httplog
+        option  dontlognull
+        timeout connect 5000
+        timeout client  50000
+        timeout server  50000
+        " >> haproxy.conf
+
+  printf '%s\n' "$Services" | (while IFS= read -r line
+  do
+    createServiceLBHaproxy "$line" "$Nodes" & 
+    done
+    wait
+  )
+
+#  echo "<SERVICES>" >> haproxy.conf
+
+  echo "listen stats *:1936
+    stats enable
+    stats uri /stats
+    stats hide-version
+    stats auth admin:Praqma" >> haproxy.conf  
+
+    $(cat *.bl >> haproxy.conf)
+    rm -f *.bl
+}
+
+function createServiceLBHaproxy(){
+  local Service=$1
+  local Nodes=$(echo $2 | tr " " "\n")
+  local line=""
+  local i=0
+
+  local ServicePort=$(getServiceNodePorts $Service "default")
+  local Endpoints=$(getServiceEndpoints $Service "default")
+
+  if [ ! "$ServicePort" == "null" ] && [ ! "$Endpoints" == "" ]; then
+    echo "
+frontend $Service
+    bind *:$ServicePort
+    mode http
+    default_backend "$Service"_BackEnds
+    " >> $Service.service.bl
+
+
+    echo "backend "$Service"_BackEnds
+    mode http
+    balance roundrobin
+    option forwardfor
+    http-request set-header X-Forwarded-Port %[dst_port]
+    http-request add-header X-Forwarded-Proto https if { ssl_fc }
+    option httpchk HEAD / HTTP/1.1\r\nHost:localhost    " >> $Service.service.bl
+
+    i=0
+    printf '%s\n' "$Nodes" | while IFS= read -r line
+    do
+      local nodeIP=$(getNodeIPs $line)
+      echo "    server "$Service"_node"$i" $nodeIP:$ServicePort check" >> $Service.service.bl
+      i=$((i+1))
+    done
+  fi
+
+}
+
+function createLBApache(){
+  local Services=$(getServices default | tr " " "\n")
+  local Nodes=$(getNodeNames)
+  local nodeIP=""
+  local line=""
 
   echo "<VirtualHost *:80>
         ProxyRequests off
@@ -16,11 +107,11 @@ function createLoadBalancer(){
         ProxyPreserveHost On
 
         IncludeOptional conf.d/*.bl" > kubernetes.services.conf
- 
+
 
   printf '%s\n' "$Services" | (while IFS= read -r line
   do
-    createServiceLB "$line" "$Nodes" & 
+    createServiceLBApache "$line" "$Nodes" &
     done
     wait
   )
@@ -48,9 +139,9 @@ function createLoadBalancer(){
 
 </VirtualHost>" >> kubernetes.services.conf
 
-} 
+}
 
-function createServiceLB(){
+function createServiceLBApache(){
   local Service=$1
   local Nodes=$(echo $2 | tr " " "\n")
   local line=""
