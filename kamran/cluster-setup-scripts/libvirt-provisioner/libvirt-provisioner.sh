4079e0019459c9008f6a00bdbcec4447f8c38d90
Kamran Azeem <kaz@praqma.net>
2016-12-02 11:56:19 +0100

    added pushd and popd, and fixed some errors.

--- kamran/cluster-setup-scripts/libvirt-provisioner/libvirt-provisioner.sh
+++ kamran/cluster-setup-scripts/libvirt-provisioner/libvirt-provisioner.sh
@@ -2,6 +2,21 @@
 # This script is used to provision nodes using libvirt.
 # You need to be root to run this.
 
+SCRIPT_PATH=$(dirname $0)
+pushd $(pwd) > /dev/null 2>&1 
+cd $SCRIPT_PATH
+
+
+function my_exit() {
+  # need to popd everytime I use 'exit N' , so I made a small function, which will do two things.
+  # popd and exit with the error code received by this function.
+  popd > /dev/null 2>&1
+  exit $1
+
+  # PS: couldn't think of a better name than 'my_exit' .
+}
+
+
 # First, source/load the functions 
 source ./functions.sh
 
@@ -13,13 +28,16 @@ if [ -f ../cluster.conf ]; then
   source ../cluster.conf
 else
   echolog "cluster.conf was not found in parent directory. Exiting ..."
-  exit 1
+  my_my_exit 1
 fi
 
 
 ############### START - Perform Sanity checks on config variables #################
 #
 #
+
+echo "Performing sanity checks ...."
+echo
 
 #if [ ! -f ../hosts ] ; then
 if [ checkHostsFile ] ; then
@@ -33,14 +51,14 @@ else
   echo "egrep -v '127.0.0.1|^#'   /etc/hosts  >  hosts"
   echo
   echo "Then add the RAM and Disk columns as described above."
-  exit 1
+  my_my_exit 1
 fi
 
 
 if [ -z "${LIBVIRT_HOST}" ] ||  [ "${LIBVIRT_HOST}" == "localhost" ] ; then
   echolog "LIBVIRT_HOST found empty (or set to localhost). Assuming the local libvirt daemon would be used."
-  echolog "Setting LIBVIRT_HOST to qemu:///system"
-  LIBVIRT_HOST=qemu:///system
+  # echolog "Setting LIBVIRT_CONNECTION to qemu:///system"
+  LIBVIRT_CONNECTION="qemu:///system"
 else
   # We have a remote host, so also check if remote user is mentioned
   if [ -z "${LIBVIRT_REMOTE_USER}" ] ; then
@@ -56,7 +74,7 @@ echolog "Setting up libvirt connection string as: ${LIBVIRT_CONNECTION}"
 if [ -z "${LIBVIRT_NETWORK_NAME}" ] ; then
   echolog "You need to provide a libvirt network name, with IP address scheme matching IPs of your k8s nodes, pecified in your hosts file."
   echo "You can do it easily using virt-manager GUI interaface. Create a NAT based network in libvirt, and when done, provide it's name as LIBVIRT_NETWORK_NAME in the config file (cluster.conf) ."
-  exit 1
+  my_my_exit 1
 else
   if [ "$(virsh net-list --name | grep ${LIBVIRT_NETWORK_NAME} )" == "${LIBVIRT_NETWORK_NAME}" ] &&  [ "$(getLibvirtNetworkState ${LIBVIRT_NETWORK_NAME})" == "active" ] ; then
     echolog "Network ${LIBVIRT_NETWORK_NAME} was found in Libvirt network list, and is in 'active' state."
@@ -64,7 +82,7 @@ else
     echolog "Network ${LIBVIRT_NETWORK_NAME} was not found in Libvirt network list. Or it is not active."
     echolog "You need to provide a libvirt network name, with IP address scheme matching IPs of your k8s nodes, pecified in your hosts file."
     echo "You can do it easily using virt-manager GUI interaface. Create a NAT based network in libvirt, and when done, provide it's name as LIBVIRT_NETWORK_NAME in the config file (cluster.conf) ."
-    exit 1
+    my_exit 1
   fi
 fi
 
@@ -74,7 +92,7 @@ else
   if [ ! -d ${VM_DISK_DIRECTORY} ] ; then
     echolog "The location provided to hold VM disks does not exist. $VM_DISK_DIRECTORY."
     echo "Please ensure that the directory exists and is owned by root:libvirt, with permissions 0775. The location needs to have at least 80 GB free disk space."
-    exit 1
+    my_exit 1
   else
     echolog "Setting up ${VM_DISK_DIRECTORY} for VM disk image storage... "
   fi
@@ -89,7 +107,7 @@ if [ -z "${HTTP_BASE_URL}" ] ; then
   echo "http://localhost:8080/"
   echo "http://server.example.org"
   echo "http://server.example.org:81"
-  exit 1
+  my_exit 1
 else
   # Lets check if URL is reachable, and that we get a 200 responce from /cdrom and /kickstart.
   # using -k with curl as user may provide a HTTPS url in cluster.conf and that URL may have self signed certs.
@@ -99,31 +117,31 @@ else
     echolog "HTTP_BASE_URL ( ${HTTP_BASE_URL} ) is accessible! Good. "
     # Lets check if /kickstart and /cdrom are accessible
     CURL_CODE_CDROM=$(curl -k -sL -w "%{http_code}\\n" "${HTTP_BASE_URL}/cdrom/.discinfo" -o /dev/null)
-    CURL_CODE_KICKSTART=$(curl -k -sL -w "%{http_code}\\n" "${HTTP_BASE_URL}/kickstart/kickstart-template.ks" -o /dev/null)
+    CURL_CODE_KICKSTART=$(curl -k -sL -w "%{http_code}\\n" "${HTTP_BASE_URL}/kickstart/kickstart.template" -o /dev/null)
     if [ ${CURL_CODE_CDROM} -eq 200 ] && [ ${CURL_CODE_KICKSTART} -eq 200 ] ; then
       echo "/cdrom and /kickstart are also accessible. Hope you have content in there!"
     else
       echolog "The /cdrom and /kickstart locations were not found through HTTP_BASE_URL  ( ${HTTP_BASE_URL}  ) ."
       echo "You need to have these two locations in the document root of your web server."
-      exit 1
+      my_exit 1
     fi
   else
     echolog "The URL specified in HTTP_BASE_URL ( ${HTTP_BASE_URL} ) is not reachable!"
-    exit 1
+    my_exit 1
   fi
 fi
 
 
 # Need to make sure that kickstart directory exists inside the parent directory. 
-# Also it needs to have a kickstart-template.ks file in it as a minimum.
+# Also it needs to have a kickstart.template file in it as a minimum.
 
-# if [ ! -f ../kickstart/kickstart-template.ks ] ; then
+# if [ ! -f ../kickstart/kickstart.template ] ; then
 if [ checkKickstart ] ; then
-  echolog "kickstart file found in kickstart/kickstart-template.ks in the parent directory."
+  echolog "kickstart file found in kickstart/kickstart.template in the parent directory."
 else
-  echolog "kickstart file not found as kickstart/kickstart-template.ks ."
-  echolog "You need to have the kickstart directory in the project root directory and also have the kickstart-template.ks file in it."
-  exit 1
+  echolog "kickstart file not found as kickstart/kickstart.template ."
+  echolog "You need to have the kickstart directory in the project root directory and also have the kickstart.template file in it."
+  my_exit 1
 fi
 
 # checkKickstart
@@ -150,6 +168,8 @@ echo
 
 # Hard coded hosts file (on purpose). Expect/Need a file named 'hosts' in the parent directory.
 HOSTS_FILE=../hosts
+
+# Hard coded kickstart directory (on purpose). I expect this directory and a template file inside it.
 KICKSTART_DIRECTORY=../kickstart
 
 LIBVIRT_NETWORK_IP=$(getLibvirtNetworkIP $LIBVIRT_NETWORK_NAME)
@@ -159,11 +179,15 @@ LIBVIRT_NETWORK_MASK=$(getLibvirtNetworkMask $LIBVIRT_NETWORK_NAME)
 # echo "Default Gateway for VMs belonging to this network is: ${LIBVIRT_NETWORK_IP}"
 # echo "Network Mask for VMs belonging to this network is: ${LIBVIRT_NETWORK_MASK}"
 
+
+# INSTALL_TIME_RAM:
+
 # Provisioning using kickstart, over the network, requires more RAM than normal.
 # So I will use 1280 MB for each VM during provisioning. As soon as node is provisioned, 
 # I will use virt-xml to edit the configuration and set the RAM of the node to the 
 # amount mentioned in the hosts file.
-VM_RAM=1280
+
+INSTALL_TIME_RAM=1280
 
 
 #
@@ -182,7 +206,14 @@ getNodeDisk controller1.example.com
 THREE_OCTETS=$(getFirstThreeOctectsOfIP ${LIBVIRT_NETWORK_IP})
 
 generateKickstartAll ${THREE_OCTETS} ${LIBVIRT_NETWORK_IP} ${LIBVIRT_NETWORK_MASK}
+
+
+echo "Running Main: createVMAll ${THREE_OCTETS} ${VM_DISK_DIRECTORY} ${LIBVIRT_NETWORK_NAME} ${HTTP_BASE_URL} ${LIBVIRT_CONNECTION}"
+createVMAll ${THREE_OCTETS} ${VM_DISK_DIRECTORY} ${LIBVIRT_NETWORK_NAME} ${HTTP_BASE_URL} ${LIBVIRT_CONNECTION}
+
+
  
 #
 #
 ###############  END - Main program #####################
+my_exit 
